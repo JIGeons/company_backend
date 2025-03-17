237eb593fa01067cf303e3782e335d48da87a6bb
JIGeons <rlrjtlrl@skuniv.ac.kr>
2025-03-18 02:28:03 +0900

    Ch4-5. 관리자 계정 로그인, JWT 토큰

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcrypt');
+const axios = require('axios');
+const jwt = require('jsonwebtoken');
 
 // Models
 const User = require('../models/User');
@@ -29,6 +31,83 @@ router.post('/signup', async (req, res) => {
   } catch (error) {
     res.status(500).json({ message: '서버 오류가 발생했습니다.'})
     console.log(error);
+  }
+});
+
+router.post('/login', async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    // password 항목을 추가로 가지고 온다.
+    const user = await User.findOne({ username }).select("+password");
+
+    if (!user) {
+      return res.status(401).json({ message: "사용자를 찾을 수 없습니다."});
+    }
+
+    // 계정이 잠겨진 상태인 경우 401 응답.
+    if (!user.isActive) {
+      return res.status(401).json({ message: "비활성화된 계정입니다. 관리자에게 문의하세요."});
+    }
+
+    // 로그인 상태인 경우
+    if (user.isLoggedIn) {
+      return res.status(401).json({ message: "이미 다른 기기에서 로그인이 되어있습니다."});
+    }
+
+    // 암호화된 비밀번호가 동일한지 비교
+    const isValidPassword = await bcrypt.compare(password, user.password);
+    if (!isValidPassword) {
+      user.failedLoginAttempts += 1;
+      user.lastLoginAttempt = new Date();
+
+      // 비밀번호를 5회 이상 틀렸을 시 계정 잠굼
+      if (user.failedLoginAttempts >= 5) {
+        user.isActive = false;
+        await user.save();
+        return res.status(401).json({ message: "비밀번호 5회 이상 틀려 계정이 비활성화 됩니다. "});
+      }
+
+      await user.save();
+      return res.status(401).json({ message: "비밀번호가 일치하지 않습니다. ", data: { remainingAttempts: 5 - user.failedLoginAttempts } });
+    }
+
+    user.failedLoginAttempts = 0;
+    user.lastLoginAttempt = new Date();
+    user.isLoggedIn = true;
+
+    try {
+      // user의 ip주소를 받아오는 방법
+      const response = await axios.get("https://api.ipify.org?format=json");
+      const ipAddress = response.data.ip; // 공인 ip
+      user.ipAddress = ipAddress;
+    } catch (error) {
+      console.log("IP 주소를 가져오던 중 오류 발생: ", error.message);
+    }
+
+    await user.save();
+
+    // JWT 토큰 발급
+    const token = jwt.sign(
+      { userId: user._id, username: user.username },
+      process.env.JWT_SECRET,
+      { expiresIn: '24h' } // 토큰 유효기간 설정
+    );
+
+    res.cookie('token', token, {
+      httpOnly: true,               // 클라이언트(JavaScript)에서 해당 쿠키에 접근 할 수 없도록 설정
+      secure: 'production',         // Https 연결에서만 쿠키를 전송하도록 설정 (서버가 운영환경에서만 Secure 옵션 활성화)
+      sameSite: 'strict',           // 외부 사이트에서의 요청에 대해 쿠키를 전송하지 않도록 설정
+      maxAge: 24 * 60 * 60 * 1000   // 쿠키의 만료 시간 설정
+    });
+
+    const userWithoutPassword = user.toObject();  // mongoDB 객체를 일반 객체로 변환
+    delete userWithoutPassword.password;
+
+    return res.status(200).json({ message: "로그인 성공", data: { user : userWithoutPassword } });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "서버 오류가 발생했습니다. "});
   }
 });
 
