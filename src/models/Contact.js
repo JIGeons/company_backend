e12b4499556dc5d7afa5c7a0843d3ec6a81e5c35
JIGeons <rlrjtlrl@skuniv.ac.kr>
2025-03-27 04:43:27 +0900

    feat: Ch6-1. MogoDB 문의글 모델 셜계

---
+++ src/models/Contact.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+
+const contactSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true, // 공백 제거
+    },
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    phone: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    message: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    status: {
+      type: String,
+      enum: ['in progress', 'pending', 'complete'],
+      default: 'in progress',
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    }
+  }, {
+    timestamps: true,
+  }
+);
+
+const Contact = mongoose.model("Contact", contactSchema);
+
+module.exports = Contact;
