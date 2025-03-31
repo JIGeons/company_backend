e6ccdf6ce806856ce1c3d2b06a51e202d6351c91
JIGeons <rlrjtlrl@skuniv.ac.kr>
2025-04-01 00:33:51 +0900

    fixed: json으로 다시 변경

--- src/middlewares/error.middleware.ts
+++ src/middlewares/error.middleware.ts
@@ -5,14 +5,14 @@ import { HttpException } from '@exceptions/httpException';
 export const ErrorMiddleware: ErrorRequestHandler = (err: Error, req: Request, res: Response, _next: NextFunction) => {
   try {
     if (err instanceof HttpException) {
-      res.status(err.status).send({
+      res.status(err.status).json({
         success: false,
         message: err.message,
       });
       return ;
     }
 
-    res.status(500).send({
+    res.status(500).json({
       success: false,
       message: '서버 오류',
       error: err.stack,
