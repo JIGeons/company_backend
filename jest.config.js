9b1f6e1114782f550c87661f661bef6b16f2c50b
JIGeons <rlrjtlrl@skuniv.ac.kr>
2025-04-18 18:51:49 +0900

    chore: database 별칭 추가

--- jest.config.js
+++ jest.config.js
@@ -30,6 +30,8 @@ module.exports = {
     "^@services/(.*)$": "<rootDir>/src/services/$1",
     "^@middlewares/(.*)$": "<rootDir>/src/middlewares/$1",
     "^@config/(.*)$": "<rootDir>/src/config/$1",
+    "@database/*": "<rootDir>/src/database/$1",
+    "@models/*": "<rootDir>/src/database/models/$1",
     "^@models/(.*)$": "<rootDir>/src/models/$1",
     "^@utils/(.*)$": "<rootDir>/src/utils/$1",
     "^@tests/(.*)$": "<rootDir>/tests/$1",
